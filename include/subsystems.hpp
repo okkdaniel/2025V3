9bcca62d74c721887817c666cca2d98c582184f6
Daniel Kaliko <danielkaliko1@gmail.com>
2025-12-31 03:19:03 -0800

    Sorting Definition

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -12,6 +12,10 @@ inline pros::Motor topRoller(7);
 inline pros::adi::DigitalOut gate('A');
 inline pros::adi::DigitalOut bottomLift('A');
 
+inline pros::Optical color('C');
+
+extern pros::Controller master;
+
 enum intakeStates
 {
     IDLE = 0,
@@ -22,6 +26,18 @@ enum intakeStates
 
 extern int intakeState;
 extern bool isHighGoal;
+extern bool isBlueAlliance;
+extern bool sorting;
 
 void intakeStateManager();
 void intakeTeleControl();
+void detectBall();
+void toggleAlliance();
+void setIntakeMotors(int bottom, int middle, int top);
+void setPiston(bool gateExtended, bool liftExtended);
+
+void setIntake(int state);
+void setHighGoal(bool high);
+void setAlliance(bool isBlue);
+
+void colorDetectionTask();
--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -4,8 +4,16 @@
 #include "pros/optical.hpp"
 #include <type_traits>
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
 int intakeState = IDLE;
 bool isHighGoal = true;
+bool isBlueAlliance = true;
+bool sorting = false;
+
+static uint32_t sortStartTime = 0;
+const uint32_t SORT_EJECT_TIME = 300; // time to eject wrong ball
+const uint32_t SORT_DELAY = 50; // delay before ejecting
 
 void intakeTeleControl()
 {
